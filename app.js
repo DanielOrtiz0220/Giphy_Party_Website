98d1df1c5f3a7716ba79c3ade7cd432890b8625d
Daniel Ortiz <daniel.ortiz@danielo-ltmu23e.internal.salesforce.com>
2022-06-16 12:36:24 -0700

    added styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const apiKey = "FiRt0VbMxNFbUK4vh5UjP47SFphUB0kA";
 const limit = 3;
+const trendingLimit = 50;
 var pages = 0;
 const rating = "";
 
@@ -11,6 +12,32 @@ let resultsElement = document.querySelector(".Results");
 console.log(gifForm);
 
 // LoadingType either loads more gifs of a type, or resets completely
+
+async function getTrending(){
+    const requestURL = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${trendingLimit}`;
+
+    // evt.preventDefault();
+    loadingType = 0;
+    console.log("heyooooo")
+
+    try{
+        // requesting data and waiting for it
+        let response = await fetch(requestURL);
+
+        // var for data that has finally arrived, must wait on the fetch
+        let responseData = await response.json();
+
+        console.log(responseData);
+
+        generateHTML(responseData, loadingType);
+
+    }
+    catch(e){
+        console.log("Error = " + e)
+    }
+    
+
+}
 
 function getResults(loadingType){
 
@@ -47,7 +74,7 @@ function getResults(loadingType){
 function generateHTML(gifData, loadingType) {
     console.log("loading type is " + loadingType)
     if (loadingType == 0){
-        pages = 0;
+        pages += 1
         resultsElement.innerHTML = '';
         document.querySelector("#loadMoreGifs").removeAttribute("hidden");
 
@@ -55,14 +82,13 @@ function generateHTML(gifData, loadingType) {
     else{
     pages += 1
     }
-    console.log(resultsElement)
-    console.log(gifData.data[0].images.downsized)
 
-    console.log(gifData.data[0].url)
     const gifDataItems = gifData.data.forEach(element => {
+        console.log(element.username)
         resultsElement.innerHTML += `
         <div class = "box">
         <img src="${element.images.fixed_height.url}" alt="${element.title}" />
+        <figcaption>${element.title}, ${element.username}</figcaption>
         </div>
     `;
     });
@@ -78,9 +104,9 @@ function handleFormSubmit(){
     return searchName;
 }
 
-//   window.onload = function(){
-//     getResults();
-
-//   }
+  window.onload = function(){
+    console.log("HEY ITS WORKING")
+    getTrending();
+  }
 
  
